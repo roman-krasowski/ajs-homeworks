1b0c18d2308a878fd518016648db224fe040ccc9
roman-krasowski <krasovskiyroman@gmail.com>
2020-11-22 23:02:44 +0300

    initial commit, problem with bundle

---
+++ modules/webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    mode:'development',
+    entry: './src/index.js',
+    devServer: {
+        contentBase: "./dist"
+    },
+    module: {
+        rules: [
+            {
+                test:/\.css$/,
+                use: [
+                    MiniCssExtractPlugin.loader, 'css-loader',
+                ],
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'development'
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].css'
+        }),
+    ],
+    
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+    }
+};
